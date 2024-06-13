bfbde548d00ccd238cfb63d395425903a02e1006
KaushikKC <kccreations1704@gmail.com>
2024-06-13 10:50:54 +0530

    solved the compile error

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,23 @@ import Swap from "@/pages/Swap";
 import Image from "next/image";
 import { useState } from "react";
 import bg from "../assests/BG.png";
+import { useAccount } from "wagmi";
 
 export default function Home() {
   const [page, setPage] = useState("Chart");
+  const account = useAccount();
+  console.log(account, "acc");
+
   return (
     <main className="bg-black h-screen">
       <Image alt="" src={bg} className="absolute left-[250px] top-[-150px]" />
       <div className="z-10 relative">
         <Navbar page={page} setPage={setPage} />
-        {page == "Chart" ? <PriceChart /> : <Swap />}
+        {account.address != undefined && page == "Swap" ? (
+          <Swap />
+        ) : (
+          <PriceChart />
+        )}
       </div>
     </main>
   );
